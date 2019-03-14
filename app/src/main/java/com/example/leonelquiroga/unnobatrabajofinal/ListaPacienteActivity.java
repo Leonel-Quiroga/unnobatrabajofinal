841fea3836cda10450bb7af6b67f1e7f6c1d01ed
Agustin <actis.agustin@gmail.com>
2019-03-13 21:17:27 -0300

    está hecha la parte de latitud y longitud, con pedido de permisos, etc..

--- app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/ListaPacienteActivity.java
+++ app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/ListaPacienteActivity.java
@@ -1,13 +1,72 @@
 package com.example.leonelquiroga.unnobatrabajofinal;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class ListaPacienteActivity extends AppCompatActivity {
+
+    private ListView listaPacientes;
+    private TextView uno, dos;
+    private LocationManager locationManager;
+    private Location loc;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_paciente);
+
+        uno = (TextView) findViewById(R.id.uno);
+        dos = (TextView) findViewById(R.id.dos);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        localizacion();
+    }
+
+    private void localizacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        // Logic to handle location object
+                        uno.setText("Latitud: " + String.valueOf(location.getLatitude()));
+                        dos.setText("Longitud: " + String.valueOf(location.getLongitude()));
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1000 /* El codigo que puse a mi request */: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    localizacion();
+                }
+            }
+        }
+    }
+
+    private void completarLista() {
+        String URL = "http://ppc.edit.com.ar:8080/resources/datos/pacientes/";
     }
 }
--- app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/MainActivity.java
+++ app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.leonelquiroga.unnobatrabajofinal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button btnListaPaciente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnListaPaciente = (Button) findViewById(R.id.btnListaPaciente);
+        btnListaPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListaPacienteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
