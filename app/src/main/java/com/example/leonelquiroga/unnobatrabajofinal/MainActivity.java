5aa5a8c264084d5bf170b5376d7e0a5a4341863f
Agustin <actis.agustin@gmail.com>
2019-03-19 00:01:41 -0300

    método cerrar sesión funcionando, probar

--- app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/MainActivity.java
+++ app/src/main/java/com/example/leonelquiroga/unnobatrabajofinal/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.leonelquiroga.unnobatrabajofinal;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    private Button btnListaPaciente;
+    private Button btnListaPaciente, btnCerrarSesion;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCerrarSesion = (Button) findViewById(R.id.btnCerrarSesion);
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedPreferences = getSharedPreferences(LoginActivity.PREFERENCE, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit().clear();
+                editor.commit();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
         btnListaPaciente = (Button) findViewById(R.id.btnListaPaciente);
